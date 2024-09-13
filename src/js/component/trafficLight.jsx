79a9f6ced2ca69d372eb2fc6bea5c522676d08bb
Alerugg <134484194+Alerugg@users.noreply.github.com>
2024-09-13 23:52:17 +0000

    fin

--- src/js/component/trafficLight.jsx
+++ src/js/component/trafficLight.jsx
@@ -1,39 +1,82 @@
-import React from "react";
-import { useState } from 'react';
-import { checkPropTypes } from "prop-types";
-import { createRoot } from "react-dom/client";
-import { FaBicycle } from "react-icons/fa";
+import React, { useState } from "react";
+import { FaBicycle } from "react-icons/fa6";
 
-let color = "blue"
+
+let backImg = "https://wallpaperaccess.com/full/1619793.jpg"
+
 const TrafficLight = () => {
+
+    const [glow, setGlow] = useState("")
+
+    const isGlowing = (text) => {
+        setGlow(text)
+    }  
+
+const activateColor = (text) => {
+    
+    return(
+
+        glow == text ? "boxShadow" : ""
+
+    )
+}
+
+const otherColor = (text) => {
+
+
+return (
+    
+    glow == text ? "boxShadow newColor" : ""
+
+)
+
+
+}
+
+const startTrafficLight = () => {
+let colors = ["red", "yellow", "green", "purple"];
+let index = 0
+
+setInterval(() => {                 // tuve ayuda aqui no me salio a mi 
+
+    index = (index + 1) % colors.length;
+    setGlow(colors[index])
+
+}, 2000)
+};
+
+
+
+
+
+
+
 
     return (
         <div className="container wrapper">
             <div className="card lightBox">
-                <div className="rectangleDiv">
-                <FaBicycle />
+                <div className={`rectangleDiv ${otherColor("purple")}`} 
+                >
+                    <FaBicycle />
                 </div>
-                <div className="circleDiv1">
-
-                </div>
-                <div className="circleDiv2">
-
-                </div>
-                <div className="circleDiv3">
-
-                </div>
+            <div  className={`circleDiv1 ${activateColor("red")}`}
+                    
+                />
+            <div className={`circleDiv2 ${activateColor("yellow")}`}
+                />
+                <div className={`circleDiv3 ${activateColor("green")}`}
+                />
             </div>
             <div className="container tube"></div>
-
+            <div className="buttonsWrapper">
+            <button onClick={() => isGlowing("red")} className="redBtn">Luz Roja </button>
+            <button  onClick={() => isGlowing("yellow")} className="yellowBtn yellow">Luz Amarilla</button>
+            <button  onClick={() => isGlowing("green")} className="greenBtn green">Luz Verde</button>
+            <button  onClick={() => isGlowing("purple")} className="newLightBtn">Nueva luz</button>
+            <button  onClick={() => startTrafficLight()} className="newLightBtn">Animar Semaforo</button>
+            </div>
         </div>
+    );
+};
 
-    )
-
-
-
-}
-// const [ color, setColor] = useState("red")
-
-
-
-export default TrafficLight
+export default TrafficLight;
--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,12 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom/client";
-
+import { FaBicycle } from "react-icons/fa6";
 // include your styles into the webpack bundle
 import "../styles/index.css";
-
-
 //import your own components
+import TrafficLight from "./component/trafficLight";
 
-import TrafficLight from "./component/trafficLight.jsx";
 //render your react application
 ReactDOM.createRoot(document.getElementById('app')).render(<TrafficLight/>);
 
